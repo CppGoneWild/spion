68c3b04d604562b23c4c2d28b333d4a99c886fbc
CppGoneWild <ludovicfalcot@gmail.com>
2020-02-29 06:42:02 -0800

    remove protocol::packet

--- src/common/protocol/types.hh
+++ src/common/protocol/types.hh
@@ -16,20 +16,12 @@ namespace protocol
 
 
 enum class data_type : std::uint8_t
-{
-	_None   = 0,
-	_Int    = 1,
-	_UInt   = 2,
-	_Double = 3,
-	_String = 4,
-	_Blob   = 5,
-
-	_Dico = 6
-};
 
 
 
 } // protocol
 } // common
+
+
 
 #endif  // COMMON_PROTOCOL_TYPES_HH_INCLUDED
